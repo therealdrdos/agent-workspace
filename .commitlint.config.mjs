c605b5279bc5bc862bb36946b6cde85e3277e08c
therealdrdos <s1monp@mailbox.org>
2026-01-17 18:46:01 +0100

    feat: add linters

---
+++ .commitlint.config.mjs
@@ -0,0 +1,24 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'ci',
+        'build',
+        'revert',
+      ],
+    ],
+    'subject-case': [2, 'always', ['lower-case', 'sentence-case']],
+    'header-max-length': [2, 'always', 100],
+  },
+};
